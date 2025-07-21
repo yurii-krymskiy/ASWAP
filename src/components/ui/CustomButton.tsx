b539df3777a7f5a87cf8fb81204ad612bdfe7e08
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-21 18:55:06 +0200

    add header, footer, custom button, router

---
+++ src/components/ui/CustomButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CustomButton = ({ text }: { text: string }) => {
+  return (
+    <button
+      className="
+        bg-[#C5C300]
+        px-5 py-2.5
+        text-[#020313]
+        rounded-[4px]
+        text-[16px]
+        font-medium
+        leading-[120%]
+        tracking-[0.02em]
+        hover:bg-[#b1af00]
+        transition-all
+        duration-200
+        ease-in-out
+        hover:shadow-md
+        active:scale-95
+        cursor-pointer
+      "
+    >
+      {text}
+    </button>
+  )
+}
+
+export default CustomButton
