cef5f586cf23e11b92e59c2092db257e549ba464
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-28 00:11:28 +0200

    add classname for gradient text in progress line

--- src/pages/Home/components/ProgressCard/StepCircle.tsx
+++ src/pages/Home/components/ProgressCard/StepCircle.tsx
@@ -2,7 +2,7 @@ import GradientTitle from "../../../../components/ui/GradientTitle";
 
 const StepCircle = ({ number }: { number: string }) => (
   <div className="w-[48px] h-[48px] flex-shrink-0 flex justify-center items-center rounded-full border border-[#FFFD00] step-circle">
-    <GradientTitle text={number} size="24" />
+    <GradientTitle text={number} classname="text-[24px]" />
   </div>
 );
 
